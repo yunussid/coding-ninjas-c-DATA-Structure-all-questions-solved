c1c00aa4ee98ba3af87fec4f69d1f49e92b66919
yunus iqbal siddique <68587652+yunussid@users.noreply.github.com>
2021-05-23 02:48:58 +0530

    Add files via upload

---
+++ TEMPLATE/Template p1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+template<typename T,typename V>
+
+class Pair{
+  T x;
+  V y;
+  public:
+   void setX(T x){
+    this->x=x;
+   }
+   T getX(){
+    return x;
+   }
+   void setY(V y){
+    this->y=y;
+   }
+   V getY(){
+    return y;
+   }
+};
+
+
+
+int main(){
+
+  Pair<Pair<int,int>, int> p2;
+  p2.setY(10);
+  Pair<int,int>p4;
+  p4.setX(5);
+  p4.setY(16);
+  p2.setX(p4);
+  cout<<p2.getY()<<" "<<p2.getX().getX()<<" "<<p2.getX().getY()<<endl;
+
+
+
+}
